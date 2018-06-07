540332ae5a3cfaf230f36a62b31d266138e2e026
Patrick Kollodzik <patrick.kollodzik@sinnerschrader.com>
2018-06-07 17:58:18 +0200

    start with request into nothing

--- src/baseServiceBinding.rs
+++
@@ -1,8 +0,0 @@
-// For now to disable the binding.rs warnings ...
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(dead_code)]
-#![allow(improper_ctypes)]
-
-include!(concat!(env!("OUT_DIR"), "/baseServicebinding.rs"));
--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,47 @@
 extern crate flik_lib;
-extern crate libc;
+extern crate reqwest;
 extern crate rpassword;
 
-use std::io::{self, Write};
 use flik_lib::app;
-
-use std::ffi::CString;
-
-mod baseServiceBinding;
-mod worktimeAccountingServiceBinding;
-
-extern "C" {
-    fn newBlueantBase() -> *mut baseServiceBinding::soap;
-    fn deleteBlueantBase(blueant_base: *mut baseServiceBinding::soap);
-}
+use std::collections::HashMap;
+use std::io::{self, Write};
 
 fn main() {
-    let username = CString::new("").unwrap();
-    let password = CString::new("uGH~mvVnLw(~bHV@eb~4A{P3-i34wkYHhjk;f3U,mq").unwrap();
-    //let mut login_params = baseServiceBinding::_baseService3__LoginRequestParameter {
-        //username: username.into_raw(),
-        //password: password.into_raw(),
-    //};
+    let mut params = HashMap::new();
+    params.insert("username", "");
+    params.insert("password", "uGH~mvVnLw(~bHV@eb~4A{P3-i34wkYHhjk;f3U,mq");
 
-    let blueant_base = unsafe { newBlueantBase() };
-    //let mut session = baseServiceBinding::_baseService3__session {
-        //sessionID: std::ptr::null_mut(),
-        //personID: 0,
-    //};
+    let client = reqwest::Client::new();
+    let res = client
+        .post("https://blueant-uat.sinnerschrader.com/blueant/services/BaseService/Login")
+        .form(&params)
+        .build()
+        .unwrap();
 
-    //let v42: std::os::raw::c_int;
-    //{
-        //v42 = unsafe {
-            //baseServiceBinding::soap_call___baseService1__Login(
-                //blueant_base,
-                //std::ptr::null(),
-                //std::ptr::null(),
-                //&mut login_params,
-                //&mut session,
-            //)
-        //};
-    //}
+    println!("Request: {:?}, {:?}", res, res.body());
 
-    //println!("Wurst {:?}, {:?}", v42, unsafe {
-        //CString::from_raw(session.sessionID)
-    //});
+    let mut result = client.execute(res).unwrap();
 
-    //let log: std::os::raw::c_int = unsafe {
-        //baseServiceBinding::soap_call___baseService1__Logout(
-            //blueant_base,
-            //std::ptr::null(),
-            //std::ptr::null(),
-            //&mut baseServiceBinding::_baseService3__LogoutRequestParameter {
-                //sessionID: session.sessionID,
-            //},
-            //&mut baseServiceBinding::__baseService1__LogoutResponse {},
-        //)
-    //};
-    //println!("Brot {:?}", log);
-
-    unsafe {
-        deleteBlueantBase(blueant_base);
-    }
+    println!("Result: {:?}", result.text().unwrap());
 
     //let sout = |a: &String| {
-        //io::stdout().write(a.as_bytes()).unwrap();
-        //io::stdout().flush().unwrap();
+    //io::stdout().write(a.as_bytes()).unwrap();
+    //io::stdout().flush().unwrap();
     //};
 
     //let serr = |a: &String| {
-        //io::stderr().write(a.as_bytes()).unwrap();
+    //io::stderr().write(a.as_bytes()).unwrap();
     //};
     //let sin = |secured: bool| -> String {
-        //if secured {
-            //rpassword::read_password().unwrap()
-        //} else {
-            //let mut input: String = String::new();
-            //io::stdin()
-                //.read_line(&mut input)
-                //.expect("reading stdin failed!");
-            //input
-        //}
+    //if secured {
+    //rpassword::read_password().unwrap()
+    //} else {
+    //let mut input: String = String::new();
+    //io::stdin()
+    //.read_line(&mut input)
+    //.expect("reading stdin failed!");
+    //input
+    //}
     //};
 
     //let result = app(std::env::args().collect(), sin, &sout, serr);
